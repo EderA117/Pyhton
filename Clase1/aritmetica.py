974b2639a6b37e260637ab88a8d17913809a09ed
EderA117 <113396905+EderA117@users.noreply.github.com>
2023-06-13 14:29:27 -0600

    Add files via upload

---
+++ Clase1/aritmetica.py
@@ -0,0 +1,9 @@
+def suma(a, b):
+    if a != None and b != None:
+        return a + b
+    else:
+        print("Envia ambos valores")
+    return 0
+
+def resta(a = 0, b = 1):
+    return a-b
---
+++ Clase1/cosas.py
@@ -0,0 +1,7 @@
+class Alumno:
+    facultad = "FES Aragon"
+
+    def __init__(self, nom, ed, carr):
+        self.__nomre = nom
+        self.__edad = carr
+        self.__carrera = "ICO"
